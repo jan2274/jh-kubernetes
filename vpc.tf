a9b162c7701026c53f2cdc6d06cf8361bb98d907
LeeJaehoon <jan2274@naver.com>
2024-10-11 20:54:07 +0900

    Add files via upload

---
+++ vpc.tf
@@ -0,0 +1,93 @@
+# vcp = 10.0.0.0/20
+# public-subnet-a = 10.0.0.0/23
+# public-subnet-c = 10.0.16.0/23
+# private-subnet-a = 10.0.32.0/23
+# private-subnet-c = 10.0.48.0/23
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "jh-vpc-main"
+  }
+}
+
+################ Subnets #################
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = var.az[count.index]
+
+  tags = {
+    Name = "jh-subnet-public-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.az[count.index % 2]
+
+  tags = {
+    Name = "jh-subnet-private-${count.index + 1}"
+  }
+}
+
+############# Internet Gateway ############
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "jh-igw-main"
+  }
+}
+
+############# Route Tables ############
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "jh-rt-public"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "jh-rt-private"
+  }
+}
+
+############# Route Tables association ############
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+
+# ################### security group #####################
+# ######### DB 보안그룹 ##########
+# resource "aws_security_group" "rds" {
+#   name        = "jh-sg-rds"
+#   description = "Security group for rds"
+#   vpc_id      = aws_vpc.main.id
+
+
+# }
