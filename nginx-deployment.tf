214d8675a1351eed45a7386d6eb1d3c873abd2b5
jan2274 <jan2274@naver.com>
2024-10-14 03:22:03 +0900

    create nginx deployment

---
+++ nginx-deployment.tf
@@ -0,0 +1,38 @@
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "nginx-deployment"
+    namespace = "default"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+
+          port {
+            container_port = 80 
+          }
+        }
+      }
+    }
+  }
+}
