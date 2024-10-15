513bcf6c0f5e90f612361481de3741ee80225aa6
jan2274 <jan2274@naver.com>
2024-10-15 18:00:11 +0900

    seperate infra, k8s

---
+++ eks-infra/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    version = "5.70"
+    region = "ap-northeast-2"
+}
+
+
--- k8s/provider.tf
+++ k8s/provider.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-    version = "5.70"
-    region = "ap-northeast-2"
-}
-
 provider "kubernetes" {
   host                   = aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
