484f18fa9f8ab092c99b055f7f0b23c4877f8b47
jan2274 <jan2274@naver.com>
2024-10-16 14:49:54 +0900

    create db

--- k8s/mysql.tf
+++ k8s/mysql.tf
@@ -1,61 +1,61 @@
-# resource "kubernetes_deployment" "mysql" {
-#   metadata {
-#     name = "mysql"
-#     labels = {
-#       app = "mysql"
-#     }
-#   }
+resource "kubernetes_deployment" "mysql" {
+  metadata {
+    name = "mysql"
+    labels = {
+      app = "mysql"
+    }
+  }
 
-#   spec {
-#     replicas = 1
+  spec {
+    replicas = 1
 
-#     selector {
-#       match_labels = {
-#         app = "mysql"
-#       }
-#     }
+    selector {
+      match_labels = {
+        app = "mysql"
+      }
+    }
 
-#     template {
-#       metadata {
-#         labels = {
-#           app = "mysql"
-#         }
-#       }
+    template {
+      metadata {
+        labels = {
+          app = "mysql"
+        }
+      }
 
-#       spec {
-#         container {
-#           name  = "mysql"
-#           image = "mysql:5.7"
+      spec {
+        container {
+          name  = "mysql"
+          image = "mysql:5.7"
           
-#           env {
-#             name  = "MYSQL_ROOT_PASSWORD"
-#             value = var.db_passwd  # MySQL root 비밀번호 설정
-#           }
+          env {
+            name  = "MYSQL_ROOT_PASSWORD"
+            value = var.db_passwd  # MySQL root 비밀번호 설정
+          }
 
-#           port {
-#             container_port = 3306  # MySQL 포트
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+          port {
+            container_port = 3306  # MySQL 포트
+          }
+        }
+      }
+    }
+  }
+}
 
-# resource "kubernetes_service" "mysql" {
-#   metadata {
-#     name = "mysql"
-#   }
+resource "kubernetes_service" "mysql" {
+  metadata {
+    name = "mysql"
+  }
 
-#   spec {
-#     selector = {
-#       app = "mysql"
-#     }
+  spec {
+    selector = {
+      app = "mysql"
+    }
 
-#     port {
-#       port        = 3306
-#       target_port = 3306
-#     }
+    port {
+      port        = 3306
+      target_port = 3306
+    }
 
-#     type = "ClusterIP"
-#   }
-# }
+    type = "ClusterIP"
+  }
+}
