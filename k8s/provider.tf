9fdb28c859f9d240f01043d293bb37c69d9058b5
jan2274 <jan2274@naver.com>
2024-10-16 13:37:24 +0900

    test2

--- k8s/provider.tf
+++ k8s/provider.tf
@@ -1,13 +1,3 @@
-provider "kubernetes" {
-  host                   = aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = var.cluster_ca_certificate
-  token                  = data.aws_eks_cluster_auth.main.token
-}
-
-data "aws_eks_cluster_auth" "main" {
-  name = aws_eks_cluster.main.name
-}
-
 # provider "kubernetes" {
 #   host                   = aws_eks_cluster.main.endpoint
 #   cluster_ca_certificate = var.cluster_ca_certificate
@@ -17,3 +7,24 @@ data "aws_eks_cluster_auth" "main" {
 # data "aws_eks_cluster_auth" "main" {
 #   name = aws_eks_cluster.main.name
 # }
+
+data "terraform_remote_state" "eks" {
+  backend = "remote"  # Terraform Cloud 백엔드를 가리킴
+  config = {
+    organization = "jh-kubernetes"  # Terraform Cloud 조직 이름
+    workspaces = {
+      name = "eks"  # EKS 클러스터가 관리되는 워크스페이스 이름
+    }
+  }
+}
+
+# Kubernetes provider 설정
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.eks.outputs.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.eks_cluster_ca)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = data.terraform_remote_state.eks.outputs.eks_cluster_name
+}
