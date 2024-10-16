31d6dfd115f966afa3e029d92c898a2db099e06e
jan2274 <jan2274@naver.com>
2024-10-16 13:32:15 +0900

    test new output

--- k8s/provider.tf
+++ k8s/provider.tf
@@ -7,3 +7,13 @@ provider "kubernetes" {
 data "aws_eks_cluster_auth" "main" {
   name = aws_eks_cluster.main.name
 }
+
+# provider "kubernetes" {
+#   host                   = aws_eks_cluster.main.endpoint
+#   cluster_ca_certificate = var.cluster_ca_certificate
+#   token                  = data.aws_eks_cluster_auth.main.token
+# }
+
+# data "aws_eks_cluster_auth" "main" {
+#   name = aws_eks_cluster.main.name
+# }
