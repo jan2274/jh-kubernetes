1cbf7440be160e6b15e308e80b3ddf15d76bdac8
jan2274 <jan2274@naver.com>
2024-10-22 01:13:56 +0900

    create output

--- eks-infra/outputs.tf
+++ eks-infra/outputs.tf
@@ -1,27 +1,27 @@
-# output "eks_cluster_name" {
-#   value = aws_eks_cluster.main.name
-# }
+output "eks_cluster_name" {
+  value = aws_eks_cluster.main.name
+}
 
-# output "eks_cluster_endpoint" {
-#   value = aws_eks_cluster.main.endpoint
-# }
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
 
-# output "eks_cluster_version" {
-#   value = aws_eks_cluster.main.version
-# }
+output "eks_cluster_version" {
+  value = aws_eks_cluster.main.version
+}
 
-# output "eks_node_group_name" {
-#   value = aws_eks_node_group.node_group.node_group_name
-# }
+output "eks_node_group_name" {
+  value = aws_eks_node_group.node_group.node_group_name
+}
 
-# output "eks_node_group_role_arn" {
-#   value = aws_iam_role.eks_node_role.arn
-# }
+output "eks_node_group_role_arn" {
+  value = aws_iam_role.eks_node_role.arn
+}
 
-# output "aws_eks_cluster" {
-#   value = aws_eks_cluster.main.name
-# }
+output "aws_eks_cluster" {
+  value = aws_eks_cluster.main.name
+}
 
-# output "eks_cluster_ca" {
-#   value = aws_eks_cluster.main.certificate_authority.0.data
-# }
+output "eks_cluster_ca" {
+  value = aws_eks_cluster.main.certificate_authority.0.data
+}
