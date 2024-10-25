b1ec7d4158b284a8346f6c5d82125b19c783ba5e
jan2274 <jan2274@naver.com>
2024-10-25 15:13:21 +0900

    create ecr

---
+++ aws-infra/ecr.tf
@@ -0,0 +1,27 @@
+resource "aws_ecr_repository" "ecr_repo" {
+  name = "ecr-codebuild"
+
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+
+# locals {
+#   region     = data.aws_region.current.name
+#   ecr_domain = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com"
+# }
+
+# output "ecr_domain" {
+#   value = local.ecr_domain
+# }
+
+# output "ecr_region" {
+#   value = local.region
+# }
+
+# output "ecr_name" {
+#   value = aws_ecr_repository.ecr_repo.name
+# }
