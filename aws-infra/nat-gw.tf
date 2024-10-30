d43cc48b8d4ff76172beef8af775f3b87e9bc2b8
jan2274 <jan2274@naver.com>
2024-10-30 16:51:31 +0900

    creat code-build, nat

--- aws-infra/nat-gw.tf
+++ aws-infra/nat-gw.tf
@@ -1,33 +1,33 @@
-# # code build에서 스크립트 실행을 위해 생성하는 인스턴스는 공인IP를 갖지 못하기 떄문에
-# # private에 인스턴스를 생성하도록 하고 public에 NAT GW를 생성하여 외부와의 통신을 허용한다.
-# # 이를 위해 NAT GW를 생성하는 코드
-# ######################
-# # Elastic IP for NAT Gateway
-# resource "aws_eip" "nat" {
-#   count = 1
-#   vpc   = true
+# code build에서 스크립트 실행을 위해 생성하는 인스턴스는 공인IP를 갖지 못하기 떄문에
+# private에 인스턴스를 생성하도록 하고 public에 NAT GW를 생성하여 외부와의 통신을 허용한다.
+# 이를 위해 NAT GW를 생성하는 코드
+######################
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  count = 1
+  vpc   = true
 
-#   tags = {
-#     Name = "jh-eip-nat"
-#   }
-# }
+  tags = {
+    Name = "jh-eip-nat"
+  }
+}
 
-# # NAT Gateway in Public Subnet
-# resource "aws_nat_gateway" "nat" {
-#   count                   = 1
-#   allocation_id           = aws_eip.nat[0].id
-#   subnet_id               = aws_subnet.public[0].id  # NAT 게이트웨이를 첫 번째 퍼블릭 서브넷에 배치
-#   connectivity_type       = "public"
+# NAT Gateway in Public Subnet
+resource "aws_nat_gateway" "nat" {
+  count                   = 1
+  allocation_id           = aws_eip.nat[0].id
+  subnet_id               = aws_subnet.public[0].id  # NAT 게이트웨이를 첫 번째 퍼블릭 서브넷에 배치
+  connectivity_type       = "public"
 
-#   tags = {
-#     Name = "jh-nat-gateway-${count.index + 1}"
-#   }
-# }
+  tags = {
+    Name = "jh-nat-gateway-${count.index + 1}"
+  }
+}
 
-# # Update Private Route Table to Use NAT Gateway
-# resource "aws_route" "private" {
-#   count                  = length(aws_subnet.private)
-#   route_table_id         = aws_route_table.private.id
-#   destination_cidr_block = "0.0.0.0/0"
-#   nat_gateway_id         = aws_nat_gateway.nat[0].id
-# }
+# Update Private Route Table to Use NAT Gateway
+resource "aws_route" "private" {
+  count                  = length(aws_subnet.private)
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat[0].id
+}
