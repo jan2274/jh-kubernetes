5a05b6d0d447d5bf03eb636da5af06650333955a
jan2274 <jan2274@naver.com>
2024-10-30 17:46:39 +0900

    change nat

--- aws-infra/nat-gw.tf
+++ aws-infra/nat-gw.tf
@@ -23,9 +23,9 @@ resource "aws_nat_gateway" "nat" {
   }
 }
 
-# # Update Private Route Table to Use NAT Gateway
-# resource "aws_route" "private" {
-#   route_table_id         = aws_route_table.private.id
-#   destination_cidr_block = "0.0.0.0/0"
-#   nat_gateway_id         = aws_nat_gateway.nat[0].id
-# }
+# Update Private Route Table to Use NAT Gateway
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat[0].id
+}
