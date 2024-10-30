86bc1db4738cd4ddf5a32a463845573424d4ef29
jan2274 <jan2274@naver.com>
2024-10-30 17:59:41 +0900

    change nat

--- aws-infra/code-build.tf
+++ aws-infra/code-build.tf
@@ -107,9 +107,9 @@ resource "aws_iam_policy" "codebuild_ecr_policy" {
         Resource = "*"
       },
       {
-        Effect: "Allow"
-        Action: "codestar-connections:UseConnection"
-        Resource: "arn:aws:codeconnections:ap-northeast-2:381492128216:connection/2c51ea2b-fa18-48ac-8b81-abc031f10822"
+        Effect = "Allow"
+        Action = "codestar-connections:UseConnection"
+        Resource = "arn:aws:codeconnections:ap-northeast-2:381492128216:connection/2c51ea2b-fa18-48ac-8b81-abc031f10822"
       }
     ]
   })
