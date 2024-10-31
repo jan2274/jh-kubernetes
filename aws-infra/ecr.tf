78108012bb5d1372cd46b36aad696754f0868bb1
LeeJaehoon <jan2274@naver.com>
2024-10-31 19:13:51 +0900

    Delete aws-infra/ecr.tf

--- aws-infra/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "ecr_repo" {
-  name = "ecr-nginx"
-
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
-}
-
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
