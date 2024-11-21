216785daccf5ab21891b5ef1c51ef594718c0d7b
jan2274 <jan2274@naver.com>
2024-11-21 23:30:53 +0900

    s3 versioning 수정

--- aws-infra/codepipeline.tf
+++ aws-infra/codepipeline.tf
@@ -1,14 +1,21 @@
-#################### CodePipeline 아티팩트 저장 S3 ####################
+#################### CodePipeline 아티팩트 저장 버킷 생성 ####################
 resource "aws_s3_bucket" "jh_s3_codepipeline" {
   bucket = "jh-s3-codepipeline"
   acl    = "private"
 
-  versioning {
-    enabled = true
-  }
+#   versioning {
+#     enabled = true
+#   }
 
   tags = {
     Name        = "CodePipelineArtifacts"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_s3_codepipeline" {
+  bucket = aws_s3_bucket.jh_s3_codepipeline.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
@@ -39,6 +46,32 @@ resource "aws_s3_bucket_policy" "jh_s3_codepipeline_policy" {
   })
 }
 
+
+#################### CodePipeline Role 생성 ####################
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "codepipeline.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+#################### 생성된 CodePipeline Role에게 CodePipeline Policy 부여 ####################
+resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
+}
+
+#################### codepipeline에게 codebuild 실행 권한 부여 ####################
 resource "aws_iam_role_policy" "codepipeline_codebuild_policy" {
   name = "CodePipelineCodeBuildPolicy"
   role = aws_iam_role.codepipeline_role.name
@@ -60,32 +93,7 @@ resource "aws_iam_role_policy" "codepipeline_codebuild_policy" {
   })
 }
 
-
-#################### 일단 제거해도 되는것으로 보임 ####################
-# resource "aws_iam_role_policy_attachment" "s3_policy" {
-#   role       = aws_iam_role.codepipeline_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-# }
-
-# resource "aws_iam_policy" "codepipeline_s3_policy" {
-#   name = "codepipeline-s3-access"
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect   = "Allow"
-#         Action   = ["s3:PutObject", "s3:GetObject", "s3:ListBucket"]
-#         Resource = [
-#           "arn:aws:s3:::jh-s3-codebuild",
-#           "arn:aws:s3:::jh-s3-codebuild/*"
-#         ]
-#       }
-#     ]
-#   })
-# }
-
-#################### 코드파이프라인한테 두 버킷한테의 접근 권한 부여 ####################
+#################### codepipeline한테 두 버킷한테의 접근 권한 부여 ####################
 resource "aws_iam_role_policy" "codepipeline_s3_policy" {
   name = "CodePipelineS3Policy"
   role = aws_iam_role.codepipeline_role.name
@@ -113,34 +121,6 @@ resource "aws_iam_role_policy" "codepipeline_s3_policy" {
   })
 }
 
-# resource "aws_iam_role_policy_attachment" "codepipeline_s3_policy_attachment" {
-#   role       = aws_iam_role.codepipeline_role.name
-#   policy_arn = aws_iam_policy.codepipeline_s3_policy.arn
-# }
-
-#################### CodePipeline Role ####################
-resource "aws_iam_role" "codepipeline_role" {
-  name = "codepipeline-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "codepipeline.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-#################### CodePipeline Policy ####################
-resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
-  role       = aws_iam_role.codepipeline_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
-}
 
 #################### CodePipeline Resource ####################
 resource "aws_codepipeline" "pipeline" {
@@ -189,22 +169,4 @@ resource "aws_codepipeline" "pipeline" {
       }
     }
   }
-
-#   stage {
-#     name = "Deploy"
-
-#     action {
-#       name             = "DeployToECR"
-#       category         = "Deploy"
-#       owner            = "AWS"
-#       provider         = "ECR"
-#       version          = "1"
-#       input_artifacts  = ["build_output"]
-
-#       configuration = {
-#         RepositoryName = aws_ecr_repository.ecr_repo.name
-#         ImageTag       = "latest"
-#       }
-#     }
-#   }
 }
